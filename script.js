1e7dc21a47939768504cad371c63e8b085c52498
sreeharidoc <144218417+sreeharidoc@users.noreply.github.com>
2023-12-13 03:22:19 -0500

    added the hover effect for the divs inside the container

--- script.js
+++ script.js
@@ -18,9 +18,31 @@ div4.classList.add("first-children")
 const div5 = document.createElement("div")
 div5.classList.add("first-children")
 
+// function changeColor(color){
+//     div1.style.background = color
+// }
+
+// div1.addEventListener("mouseover",() => {
+//     changeColor("blue")
+// })
+
+// div1.addEventListener("mouseout",() =>{
+//     changeColor("white")
+// })
+
+
+
 container.appendChild(div1)
 container.appendChild(div2)
 container.appendChild(div3)
 container.appendChild(div4)
 container.appendChild(div5)
 body.appendChild(container)
+
+var divisions = document.querySelectorAll(".first-children")
+
+divisions.forEach(function(element){
+    element.addEventListener("mouseover",()=>{
+        element.style.background = "blue"
+    })
+})
